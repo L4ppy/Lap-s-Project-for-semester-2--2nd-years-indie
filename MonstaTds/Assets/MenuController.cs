7668fb8a0907c2b06bd55afd7e91d2cf50d8561f
L4ppy <114556422+L4ppy@users.noreply.github.com>
2024-06-18 12:38:04 +0700

    Revert "t3"
    This reverts commit 81b974ceb567adbcc4cabe2b651ef7d1c39bb3e7.

--- MonstaTds/Assets/MenuController.cs
+++ MonstaTds/Assets/MenuController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
-    public Transform demoCamera;
-    public float rotationSpeed = 10f;
-
-    private void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        demoCamera.RotateAround(Vector3.zero, Vector3.up, rotationSpeed * Time.deltaTime);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
